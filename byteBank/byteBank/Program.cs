429541377b94ec78c8d066e48fbae1c3bc49fbae
Hudson Retonde <hudsonsretonde@gmail.com>
2023-03-04 20:44:55 -0300

    Criando a primeira classe a atribuindo valor as suas propriedades.

---
+++ byteBank/byteBank/Program.cs
@@ -0,0 +1,9 @@
+﻿using byteBank;
+
+ContaCorrente contaDoHudson = new ContaCorrente();
+contaDoHudson.titular = "Hudson Retonde";
+contaDoHudson.numero_agencia = 15;
+contaDoHudson.conta = "1010-X";
+contaDoHudson.saldo = 100;
+
+Console.WriteLine($"Saldo da conta do Hudson é = {contaDoHudson.saldo} .");
