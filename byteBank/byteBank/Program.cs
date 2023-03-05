06d319d8d27234ad81464ba90eeb45ff03c2c458
Hudson Retonde <hudsonsretonde@gmail.com>
2023-03-04 21:17:39 -0300

    Metodo sacar, depositar e transferir

--- byteBank/byteBank/Program.cs
+++ byteBank/byteBank/Program.cs
@@ -7,3 +7,20 @@ contaDoHudson.conta = "1010-X";
 contaDoHudson.saldo = 100;
 
 Console.WriteLine($"Saldo da conta do Hudson é = {contaDoHudson.saldo} .");
+
+contaDoHudson.Depositar(100);
+
+Console.WriteLine($"Saldo do Hudson após receber R$ 100,00 reais é R$ {contaDoHudson.saldo}");
+
+ContaCorrente contaDaMaria = new ContaCorrente();
+contaDaMaria.titular = "Maria Pereira";
+contaDaMaria.numero_agencia = 17;
+contaDaMaria.conta = "1010-Y";
+contaDaMaria.saldo = 350;
+
+Console.WriteLine($"Saldo da conta da Maria é = {contaDaMaria.saldo} .");
+
+contaDoHudson.Transferir(50, contaDaMaria);
+
+Console.WriteLine($"Saldo da conta do Hudson é = {contaDoHudson.saldo} .");
+Console.WriteLine($"Saldo da conta da Maria é = {contaDaMaria.saldo} .");
